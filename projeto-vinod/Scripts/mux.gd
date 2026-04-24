605db39c511a923f65cd39cda1abf56940622066
Wuogui <133606772+Wuogui@users.noreply.github.com>
2026-04-24 11:57:12 -0300

    (feat): MUX (ainda falta pegar dado)

---
+++ projeto-vinod/Scripts/mux.gd
@@ -0,0 +1,12 @@
+extends BlocoBase
+
+func quando_clica_botao(controle : String):
+	# Falta pegar dado
+	
+	# Dependendo do controle, seleciona uma saída (0 - dado 1, 1 - dado 2);
+	if (controle == '0'):
+		dado_armazenado = dado1
+	elif (controle == '1'):
+		dado_armazenado = dado2
+	else:
+		print("Erro no MUX: Controle inválido: " + controle)
