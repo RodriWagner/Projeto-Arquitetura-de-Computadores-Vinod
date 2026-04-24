dfb65248d2aa54feba09664fadbb403bc558ce20
Wuogui <133606772+Wuogui@users.noreply.github.com>
2026-04-24 11:56:49 -0300

    (doc): melhoria na doc do bloco base

--- projeto-vinod/Scripts/bloco_base.gd
+++ projeto-vinod/Scripts/bloco_base.gd
@@ -9,7 +9,11 @@ var dado2 : String = ""
 ## Dado que esse bloco irá enviar para os outros blocos
 @export var dado_armazenado : String = ""
 
-# Essa função vai estar conectada a um sinal q será emitido pelo botão
+## Essa função vai estar conectada a um sinal q será emitido pelo botão
+## É dever essa função, a partir de um controle, executar a seguinte lista:
+## 1) Pegar dado
+## 2) Processa dado 
+## 3) Armazena dado em dado_armazenado
 func quando_clica_botao(controle : String):
 	# Pega dado
 	# Processa
