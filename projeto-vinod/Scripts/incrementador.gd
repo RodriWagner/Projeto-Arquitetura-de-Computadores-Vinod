7c2947e9771ade902c0afabb767a8698feaf16ae
Thiago L.C.C. <thiagoluizcastro2006@gmail.com>
2026-05-25 20:40:37 -0300

    Create incrementador

---
+++ projeto-vinod/Scripts/incrementador.gd
@@ -0,0 +1,20 @@
+extends ComponenteBase
+
+func agir():
+	
+	if controle == '0':
+		saida = entrada_1
+	
+	elif controle == '1':
+		var temp: int = entrada_1.bin_to_int()
+		temp += 1
+		
+		var result: String = ""
+		while temp > 0:
+			result = str(temp & 1) + result
+			temp = temp >> 1
+		
+		saida = result
+	
+	else:
+		print("Erro no incrementador: Controle inválido: " + controle)
