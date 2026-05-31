3059f8e0acc9aad0704667091c2565750c14ad2c
Wuogui <rodrigo.wag.s@gmail.com>
2026-05-31 15:29:03 -0300

    feat(Interface): botao de pausar e despausar funcional

--- projeto-vinod/Scripts/ScriptsUI/Interface.gd
+++ projeto-vinod/Scripts/ScriptsUI/Interface.gd
@@ -10,8 +10,26 @@ extends Control
 @export_subgroup("Gerenciador de Tempo")
 @export var gerenciador : Gerenciador;
 
+var iniciou : bool = false
+
 func _on_iniciar_simulação_pressed() -> void:
 	# Escreve programa na memória
 	memoria_principal.escreve_programa_em_memoria(leitor_de_macro.trata_texto(leitor_de_texto.text));
 	# Inicia gerenciador de tempo
 	gerenciador.timer_dos_subciclos.start()
+	iniciou = true
+
+
+func _on_avança_pressed() -> void:
+	if(!gerenciador.timer_ativado):
+		gerenciador.atualiza_subciclos();
+
+
+func _on_pausa_pressed() -> void:
+	var pause = $"PanelContainer/HBoxContainer/Botões e Texto/PanelContainer2/VBoxContainer/HBoxContainer3/pausa"
+	if(gerenciador.timer_ativado and iniciou):
+		gerenciador.timer_ativado = false
+		pause.text = "DESPAUSA"
+	else:
+		gerenciador.timer_ativado = true
+		pause.text = "PAUSA"
