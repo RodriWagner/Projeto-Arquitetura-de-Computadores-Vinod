8a55ffdceb8fd2a41ef38269c8e815830476dd71
RodriWagner <133606772+RodriWagner@users.noreply.github.com>
2026-06-20 14:58:00 -0300

    fix(conector_mar_mbr): conector agora funcionando devidamente
    ele tava atualizando saida e entrada e não o endereço de acesso

---
+++ projeto-vinod/Cenas/Cenas de lógica interna/conector_mar_mbr.gd
@@ -0,0 +1,8 @@
+extends Node2D
+@export var mar : ComponenteBase
+@export var mbr : MBR
+
+func _process(delta: float) -> void:
+	print("concmarmbr mbr_init_end: "+mbr.endereco_memoria)
+	mbr.endereco_memoria = mar.saida
+	print("concmarmbr mbr_final_end: "+mbr.endereco_memoria)
